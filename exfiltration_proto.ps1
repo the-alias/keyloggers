fa0b9555512562cc1ddcec850ff4bf698d5093f0
the-alias <39953366+the-alias@users.noreply.github.com>
2023-07-16 14:59:56 +0300

    Create exfiltration_proto.ps1

---
+++ exfiltration_proto.ps1
@@ -0,0 +1,36 @@
+Function Send-TCPMessage { 
+    Param ( 
+            [Parameter(Mandatory=$true, Position=0)]
+            [ValidateNotNullOrEmpty()] 
+            [string] 
+            $EndPoint
+        , 
+            [Parameter(Mandatory=$true, Position=1)]
+            [int]
+            $Port
+        , 
+            [Parameter(Mandatory=$true, Position=2)]
+            [string]
+            $Message
+    ) 
+    Process {
+        # Setup connection 
+        $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+        $Address = [System.Net.IPAddress]::Parse($IP) 
+        $Socket = New-Object System.Net.Sockets.TCPClient($Address,$Port) 
+    
+        # Setup stream wrtier 
+        $Stream = $Socket.GetStream() 
+        $Writer = New-Object System.IO.StreamWriter($Stream)
+
+        # Write message to stream
+        $Message | % {
+            $Writer.WriteLine($_)
+            $Writer.Flush()
+        }
+    
+        # Close connection and stream
+        $Stream.Close()
+        $Socket.Close()
+    }
+}
